c516ce993a397d39c6999428f16b410607d4bce2
Alexander Demchenko <alexandersd3306@gmail.com>
2025-01-20 22:49:30 +0500

    lab2 commite

---
+++ week2/1.py
@@ -0,0 +1,271 @@
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# a = 200
+# b = 33
+
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+
+
+
+
+# ##
+# print((6 + 3) - (6 + 3))
+
+
+# ()	Parentheses
+# ** Exponentiation
+# +x - x  ~x	Unary plus, unary minus, and bitwise NOT
+# * / // %	Multiplication, division, floor division, and modulus
+# + - Addition and subtraction
+# << >> Bitwise left and right shifts
+# &	Bitwise AND
+# ^	Bitwise XOR
+# |	Bitwise OR
+# == != > >= < <= is is not in not in Comparisons, identity, and membership operators
+# not Logical NOT
+# and AND
+# or OR
+
+
+
+# #
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+
+
+#   fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+
+
+# Method	Description
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
+
+# #
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[1])
+
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+
+# print(x)
+
+
+# fruits = ("apple", "banana", "cherry")
+
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+
+#   tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+
+# #
+
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+#   thisset = {"apple", "banana", "cherry"}
+
+# thisset.add("orange")
+
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.remove("banana")
+
+# print(thisset)
+
+
+
+# thisset = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+
+
+#   set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+
+# set3 = set1.union(set2)
+# print(set3)
+
+
+
+# #
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict["model"]
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict["year"] = 2018
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict["color"] = "red"
+# print(thisdict)
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.pop("model")
+# print(thisdict)
+
+
+
+# for x in thisdict:
+#   print(thisdict[x])
+
+
+#   thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# mydict = thisdict.copy()
+# print(mydict)
+
+
+# myfamily = {
+#   "child1" : {
+#     "name" : "Emil",
+#     "year" : 2004
+#   },
+#   "child2" : {
+#     "name" : "Tobias",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Linus",
+#     "year" : 2011
+#   }
+# }
+
+
+# #
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+
+  
+# if a > b: print("a is greater than b")
+
+
+# #
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+
+
+#   #
+
+#   fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+#   for x in "banana":
+#   print(x)
+
+
+#   fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+
