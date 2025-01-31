7e07e3bd97d61a8febef18de704c91a70187f541
Alexander Demchenko <alexandersd3306@gmail.com>
2025-01-31 13:49:39 +0500

    6task

--- week3/classes/6.py
+++ week3/classes/6.py
@@ -8,6 +8,6 @@ def is_prime(n):
 
 
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 24, 29]
-prime_numbers = list(filter(lambda x: is_prime(x),      ))
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
 
 print("Prime numbers:", prime_numbers)
